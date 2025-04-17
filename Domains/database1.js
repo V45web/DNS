2e19f0357488fff9fa49f39e737af0825caf77c0
V 45 <sanagkerkar45@gmail.com>
2025-04-17 23:33:52 +0530

    Create database1.js

---
+++ Domains/database1.js
@@ -0,0 +1,16 @@
+const domain = {
+  //domain
+    "sanag.dev": {
+      ip: "190.628.199",
+      account_id: "1001628027"
+    },
+    "harsh.dev": {
+      ip: "190.628.199",
+      account_id: "1002828027"
+    },
+    "san.dev": {
+      ip: "190.628.199",
+      account_id: "1001638027"
+    }
+  //end
+};
